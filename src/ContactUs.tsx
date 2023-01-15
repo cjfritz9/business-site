f01faa97b06b85c90c6cdc70d5f8014c993dca03
CJ <cjfritz9@gmail.com>
2023-01-15 16:40:40 -0700

    more google api testing

--- src/ContactUs.tsx
+++ src/ContactUs.tsx
@@ -1,64 +1,83 @@
-import React, { SyntheticEvent, useState } from "react";
+import React, { SyntheticEvent, useState } from 'react';
 // import axios from "axios";
 
 const ContactUs: React.FC = () => {
-  const [submitError, setSubmitError] = useState<string>("");
+  const [submitError, setSubmitError] = useState<string>('');
 
   const getIsFormValid = (): Boolean => {
-    const formInputs = document.getElementsByClassName("cfi") as HTMLCollectionOf<HTMLInputElement>;
+    const formInputs = document.getElementsByClassName(
+      'cfi'
+    ) as HTMLCollectionOf<HTMLInputElement>;
     console.log(formInputs);
-    
+
     for (const input of formInputs) {
       if (!input.value.length) {
-        setSubmitError("Please fill out the entire form!")
+        setSubmitError('Please fill out the entire form!');
         return false;
       } else {
         return true;
       }
     }
     return false;
-  }
+  };
 
   const handleSubmit = async (e: SyntheticEvent): Promise<any> => {
     e.preventDefault();
     if (getIsFormValid()) {
-      const { value: user } = document.getElementById("cf-name")! as HTMLInputElement;
-      const { value: email } = document.getElementById("cf-email")! as HTMLInputElement;
-      const { value: message } = document.getElementById("cf-msg")! as HTMLInputElement;
+      const { value: user } = document.getElementById(
+        'cf-name'
+      )! as HTMLInputElement;
+      const { value: email } = document.getElementById(
+        'cf-email'
+      )! as HTMLInputElement;
+      const { value: message } = document.getElementById(
+        'cf-msg'
+      )! as HTMLInputElement;
       const response = await fetch(
-        "https://gmail.googleapis.com/upload/gmail/v1/users/dev.cjfritz@gmail.com/messages/send",
-        {method: "POST",
-        headers: {
-        "Content-Type": "application/json"
-      },
-        body: JSON.stringify({
-          user,
-          email,
-          message
-        })
-      });
+        'https://jeege-ceana.netlify.app/https://gmail.googleapis.com/upload/gmail/v1/users/dev.cjfritz@gmail.com/messages/send',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify({
+            user,
+            email,
+            message
+          })
+        }
+      );
       const result = response.json();
+      console.log(result);
       return result;
     }
-  }
+  };
 
   const resetError = (): void => {
-    setSubmitError("");
-  }
+    setSubmitError('');
+  };
 
-  document.addEventListener("mousedown", resetError);
+  document.addEventListener('mousedown', resetError);
 
   return (
     <div>
-      <form className="contact-form" onSubmit={(e: SyntheticEvent) => handleSubmit(e)}>
-        <input id="cf-name" className="cfi" placeholder="Your Name"></input>
-        <input id="cf-email" className="cfi" placeholder="Your Email"></input>
-        <input id="cf-msg" className="cfi" placeholder="Ask Us Anything!"></input>
-        <button className="cf-submit">Send</button>
+      <form
+        className='contact-form'
+        onSubmit={(e: SyntheticEvent) => handleSubmit(e)}
+      >
+        <input id='cf-name' className='cfi' placeholder='Your Name'></input>
+        <input id='cf-email' className='cfi' placeholder='Your Email'></input>
+        <input
+          id='cf-msg'
+          className='cfi'
+          placeholder='Ask Us Anything!'
+        ></input>
+        <button className='cf-submit'>Send</button>
       </form>
       <div>{submitError}</div>
     </div>
-  )
-}
+  );
+};
 
 export default ContactUs;
